9fca7c68368f6d8d3ebec4236c04b842b70dbe85
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-04-04 14:18:29 +0200

    minor removal of skrypt.html until I figure out where I had the paper.js tutorial

--- datingscript.js
+++ datingscript.js
@@ -1,1 +1,8 @@
 /*datingscript.js for datingSkillFeed.html*/
+
+console.log("begin RUNNING\n.\n.\n.");
+
+/*Add an event listener waiting for the WINDOW to LOAD, and when that happens, it triggers our function*/
+window.addEventListener("load", function() {
+   console.log("the PAGE has been LOADED");
+});
