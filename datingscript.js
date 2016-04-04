b624ee707953844e83844fb24c61fed76bb5570d
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-04-04 20:24:36 +0200

    5. modifying the DOM (skillfeed)

--- datingscript.js
+++ datingscript.js
@@ -1,8 +1,19 @@
 /*datingscript.js for datingSkillFeed.html*/
 
 console.log("begin RUNNING\n.\n.\n.");
+function butonKlikd() {
+    console.log("Butn wuz klikd.");
+    console.log(results.innerHTML);
+}
 
 /*Add an event listener waiting for the WINDOW to LOAD, and when that happens, it triggers our function*/
 window.addEventListener("load", function() {
-   console.log("the PAGE has been LOADED");
+    
+    console.log("the PAGE has been LOADED");
+    var results = document.getElementById("results");
+    var searchBtn = document.getElementById("searchBtn");
+    console.log(searchBtn);
+     /*And now we listen for the CLICK event by attaching an EventListener to our SearchBtn variable*/
+    searchBtn.addEventListener("click", butonKlikd);
+  
 });
