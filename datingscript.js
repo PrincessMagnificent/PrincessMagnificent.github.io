8778477f974e8d1594d888393e67876b5df9884b
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-04-24 19:18:56 +0200

    sunday update

--- datingscript.js
+++ datingscript.js
@@ -2,6 +2,52 @@
 
 console.log("begin RUNNING\n.\n.\n.");
 
+/*This is our database of users. in JSON form. Could also get it from somewhere else, but for now...*/
+var users = [
+    {
+        name: "Akemi Homura",
+        gender: "F",
+        hobby: "pets",
+        avatar: "images/homura.gif"
+    },
+    
+    {
+        name: "Kyoko the Kyoko",
+        gender: "F",
+        hobby: "pets",
+        avatar: "images/kyoko.gif"
+    },
+    
+    {
+        name: "Madoka Meguca",
+        gender: "F",
+        hobby: "pets",
+        avatar: "images/madoka.gif"
+    },
+    
+    {
+        name: "Mammi Mami",
+        gender: "F",
+        hobby: "pets",
+        avatar: "images/mami.gif"
+    },
+    
+    {
+        name: "Sayaka Zombie",
+        gender: "F",
+        hobby: "pets",
+        avatar: "images/sayaka.gif"
+    },
+];
+
+var usersLength = users.length;
+
+for(var i = 0; i < usersLength; ++i) {
+    resultsHTML += users[i].name;
+}
+
+var resultsHTML = "";
+
 /*This function is what happens h+when the Search button is clicked, it is attached to the actual button at the bottom of the file*/
 function butonKlikd() {
     console.log("Butn wuz klikd.");
@@ -18,8 +64,8 @@ function butonKlikd() {
     var gender = genderField.options[s].value;
     var genderOption = genderField.option[s];
     /*option[s] gives us the entire html of the option, if we go .value it only gives us the value defined IN the HTML*/
-    console.log("genderOption ",genderOption);
-    console.log("gender ",gender);
+    console.log("genderOption ", genderOption);
+    console.log("gender ", gender);
 }
 
 /*Add an event listener waiting for the WINDOW to LOAD, and when that happens, it triggers our function*/
@@ -31,6 +77,4 @@ window.addEventListener("load", function() {
     console.log(searchBtn);
      /*And now we listen for the CLICK event by attaching an EventListener to our SearchBtn variable*/
     searchBtn.addEventListener("click", butonKlikd);
-  
-     
 });
---
+++ pakManScript.js
@@ -0,0 +1,3 @@
+/*pakManScript.js
+
+from HTML5 Game from scratch step by step learning JavaScript - Shortcut*/
