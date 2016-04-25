d9be829bf06d5c600b063cbaa59b3c84dde30ffe
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-04-25 13:35:55 +0200

    pakmanfoolery 1

--- pakManScript.js
+++ pakManScript.js
@@ -1,3 +1,12 @@
 /*pakManScript.js
 
 from HTML5 Game from scratch step by step learning JavaScript - Shortcut*/
+
+var canvas = doc.getElementById("canvasSpace");
+
+/*ctx will let us interact with canvas*/
+var ctx = canvas.getContext("2d");
+
+/*Gotta make sure that the page has loaded*/
+
+ctx.fillText("CLOSEtheWORLD",30,150);
