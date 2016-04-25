c1de3192f393f9329ac3add2727c5a70413c8279
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-04-25 21:35:50 +0200

    blue text, not canvas?

--- pakManScript.js
+++ pakManScript.js
@@ -9,11 +9,35 @@ var ctx = canvas.getContext("2d");*/
 
 /*This method is for adding the canvas TO the html with js. So we know it's in there because we created it IN our script, you see?*/
 var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
+var context = canvas.getContext("2d");
 canvas.height = 480;
 canvas.width = 640;
+
+/*We will now load the image with all our sprites*/
+mainImage = new Image();
+mainImage.ready = false; /*Gonna change this to true once it's loaded, and on load it will run the function checkReady to check...if it's ready*/
+mainImage.onload = checkReady();
+mainImage.src = "pac.png";
+
+function checkReady() {
+    this.ready = true;
+    playgame();
+}
+
+/*This will be the game loop*/
+function playgame() {
+    render();
+}
+
+/*here we output the content onto the canvas*/
+function render() {
+    context.fillStyle = "blue";
+    context.fillRect = (0, 0, context.width, context.height);
+    console.log("renderFunctionRan");
+}
+
 document.body.appendChild(canvas);
 
-ctx.fillText("CLOSEtheWORLD\nOPENtheNEXT",30,150);
+/*context.fillText("CLOSEtheWORLD\nOPENtheNEXT",30,150);*/
 
 console.log("javascript executed.");
