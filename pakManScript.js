53e386cd4d7e41fdbc077a852424ef563a6ed234
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-04-25 18:30:23 +0200

    java2

--- pakManScript.js
+++ pakManScript.js
@@ -2,11 +2,18 @@
 
 from HTML5 Game from scratch step by step learning JavaScript - Shortcut*/
 
-var canvas = doc.getElementById("canvasSpace");
-
+/*
+var canvas = document.getElementById("canvasSpace");
+var ctx = canvas.getContext("2d");*/
 /*ctx will let us interact with canvas*/
+
+/*This method is for adding the canvas TO the html with js. So we know it's in there because we created it IN our script, you see?*/
+var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
+canvas.height = 480;
+canvas.width = 640;
+document.body.appendChild(canvas);
 
-/*Gotta make sure that the page has loaded*/
+ctx.fillText("CLOSEtheWORLD\nOPENtheNEXT",30,150);
 
-ctx.fillText("CLOSEtheWORLD",30,150);
+console.log("javascript executed.");
